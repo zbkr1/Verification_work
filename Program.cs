da58a31bc64ed616bea7e4b96d9cccb25742d3c9
zbkr1p <zbkr1@rambler.ru>
2022-09-16 11:58:07 +0300

    programm

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿string? yesNo = "";
+while (yesNo.ToLower() != "yes" && yesNo.ToLower() != "no")
+{
+  Console.Write("Желаете вводить строки вручную? (yes/no): ");
+  yesNo = Console.ReadLine();
+}
+string[] arrayOfStrings = new string[]  {};
+if (yesNo.ToLower() == "yes") {
+  int n = InputNumbers("Введите количество элементов массива: ");
+  arrayOfStrings = new string[n];
+  for (int i = 0; i < arrayOfStrings.Length; i++)
+  {
+    Console.Write($" Введите {i+1} строку: ");
+    arrayOfStrings[i] = Console.ReadLine();
+  }
+}
+else 
+{
+  arrayOfStrings = new string[]  {"hello", "2", "world", ":-)"};
+}
+int lengthLimit = 3;
+int numbersItems = CheckArray(arrayOfStrings, lengthLimit);
+string[] newArrayOfStrings = new string[numbersItems];
+FillNewArray(arrayOfStrings, newArrayOfStrings, lengthLimit);
+Console.WriteLine($"{PrintArray(newArrayOfStrings)}");
+void FillNewArray(string[] oldArray, string[] newArray, int lengthLimit)
+{
+  int temp = 0;
+  for (int i = 0; i < oldArray.Length; i++)
+  {
+    if (oldArray[i].Length <= lengthLimit)
+    {
+      newArray[temp] = oldArray[i];
+      temp++;
+    }
+  }
+}
+int CheckArray(string[] array, int lengthLimit)
+{
+  int result = 0;
+  for (int i = 0; i < array.Length; i++)
+  {
+    if (array[i].Length <= lengthLimit) result++;
+  }
+  return result;
+}
+string PrintArray(string[] array)
+{ 
+  string result = string.Empty;
+  result = "[ ";
+  for (int i = 0; i < array.Length; i++)
+  {
+    result += $"{array[i],1}";
+    if (i < array.Length - 1) result += ", ";
+  }
+  result += " ]";
+  return result;
+}
+int InputNumbers(string input)
+{
+  Console.Write(input);
+  int output = Convert.ToInt32(Console.ReadLine());
+  return output;
+}
+
